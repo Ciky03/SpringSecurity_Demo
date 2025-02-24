991e415ebbc722747c22eaed72e0e1a24b1c0a41
Ciky03 <Ciky03@users.noreply.github.com>
2025-02-24 19:12:46 +0800

    引入mysql和mybatisPlus

--- springsecurityQuickStart/src/main/java/cloud/ciky/SecurityApplication.java
+++ springsecurityQuickStart/src/main/java/cloud/ciky/SecurityApplication.java
@@ -1,12 +1,13 @@
 package cloud.ciky;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("cloud.ciky.mapper")
 public class SecurityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class,args);
     }
---
+++ springsecurityQuickStart/src/main/java/cloud/ciky/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package cloud.ciky.mapper;
+
+import cloud.ciky.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Author: ciky
+ * @Description: User表的mapper
+ * @DateTime: 2025/2/24 18:59
+ **/
+public interface UserMapper extends BaseMapper<User> {
+}
