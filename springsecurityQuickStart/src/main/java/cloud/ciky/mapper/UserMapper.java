78816bfc2e34ed903968acb501d115c2a8585407
Ciky03 <Ciky03@users.noreply.github.com>
2025-02-24 19:42:16 +0800

    引入springboot test测试依赖,测试mapper

--- springsecurityQuickStart/src/main/java/cloud/ciky/mapper/UserMapper.java
+++ springsecurityQuickStart/src/main/java/cloud/ciky/mapper/UserMapper.java
@@ -2,11 +2,13 @@ package cloud.ciky.mapper;
 
 import cloud.ciky.domain.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @Author: ciky
  * @Description: User表的mapper
  * @DateTime: 2025/2/24 18:59
  **/
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
---
+++ springsecurityQuickStart/src/test/java/cloud/ciky/MapperTest.java
@@ -0,0 +1,27 @@
+package cloud.ciky;
+
+import cloud.ciky.domain.User;
+import cloud.ciky.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+/**
+ * @Author: ciky
+ * @Description: Mapper测试
+ * @DateTime: 2025/2/24 19:13
+ **/
+@SpringBootTest
+public class MapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testUserMapper(){
+        List<User> users = userMapper.selectList(null);
+        System.out.println(users);
+    }
+}
