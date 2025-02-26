8e2149844f003833693b96e2b75d6d4bb47f9460
Ciky03 <Ciky03@users.noreply.github.com>
2025-02-26 19:50:14 +0800

    测试跨域接口

--- springsecurityQuickStart/src/main/java/cloud/ciky/controller/HelloController.java
+++ springsecurityQuickStart/src/main/java/cloud/ciky/controller/HelloController.java
@@ -1,5 +1,6 @@
 package cloud.ciky.controller;
 
+import cloud.ciky.domain.ResponseResult;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,10 @@ public class HelloController {
     @PreAuthorize("hasAuthority('system:test:list')")
     public String hello(){
         return "hello";
+    }
+
+    @RequestMapping("/testCors")
+    public ResponseResult testCors(){
+        return new ResponseResult(200,"testCors");
     }
 }
